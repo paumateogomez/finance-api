e453296dfd8886bc93ec71f3ce163a61275e3434
Pau Mateo Gómez <paumateogomez06@gmail.com>
2026-06-05 17:38:12 +0200

    Fix transaction ID generation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
 from src.models.transaction import Transaction
 app = FastAPI()
 
@@ -14,12 +15,33 @@ def home():
 def get_transactions():
     return transactions
 
+@app.get("/transactions/{transaction_id}")
+def get_transaction(transaction_id: int):
+    for transaction in transactions:
+        if transaction["id"] == transaction_id:
+            return transaction
+    raise HTTPException(status_code=404, detail="Transaction not found")
 
 @app.post("/transactions")
 def add_transaction(transaction: Transaction):
-    transactions.append(transaction.model_dump())
+    new_transaction = transaction.model_dump()
+    if not transactions:
+        new_transaction["id"] = 1
+    else:
+        max_id = max(transaction["id"] for transaction in transactions)
+        new_transaction["id"] = max_id + 1
+
+    transactions.append(new_transaction)
 
     return {
         "message": "Transaction added successfully",
-        "transaction": transaction
+        "transaction": new_transaction
     }
+
+@app.delete("/transactions/{transaction_id}")
+def delete_transaction(transaction_id: int): 
+    for transaction in transactions:
+        if transaction["id"] == transaction_id:
+            transactions.remove(transaction)
+            return {"message": "Transaction deleted successfully"}
+    raise HTTPException(status_code=404, detail="Transaction not found")   
